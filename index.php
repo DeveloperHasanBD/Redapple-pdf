44847dd7aa57cfa29287847d75b656e34f5bacec
Developer Hasan <67751842+DeveloperHasanBD@users.noreply.github.com>
2024-10-21 10:42:21 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-<form class="d-none" action="<?php echo esc_url(admin_url('admin-post.php')); ?>" method="POST">
-    <input type="hidden" name="action" value="generate_sample_pdf">
+<form  action="<?php echo esc_url(admin_url('admin-post.php')); ?>" method="POST">
+    <input type="hidden" name="action" value="generate_redapple_pdf">
     <input type="hidden" name="product_id" value="<?php echo get_the_ID(); ?>">
-    <input id="dkr-product-pdf" type="submit" name="submit" value="SCARICA SCHEDA PDF">
+    <input type="submit" name="submit" value="SCARICA SCHEDA PDF">
 </form>
